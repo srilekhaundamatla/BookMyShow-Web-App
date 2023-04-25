e4965926f7f432c9cf83b87dd4aff6f1c2878117
srilekhaundamatla <122496949+srilekhaundamatla@users.noreply.github.com>
2023-04-25 15:12:10 -0400

    Delete Middle_Frame.js

--- Middle_Frame.js
+++
@@ -1,9 +0,0 @@
-
-var username=localStorage.getItem('username');
-console.log(localStorage.getItem('username'));
-if(localStorage.getItem('username')==null){
-    document.getElementById('user').innerHTML="LOGIN/SIGNUP";
-}
-else{
-    document.getElementById('user').innerHTML=username;
-}
